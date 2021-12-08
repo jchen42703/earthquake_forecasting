b6cab7dcd71a1ac1af57de480369ed971a20173e
Joseph Chen <jchen42703@gmail.com>
2021-12-07 21:28:05 -0500

    dynamic ports for dev url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,8 @@
-const devUrl = "http://localhost:8000";
+const port = process.env.REACT_APP_API_PORT;
+const devUrl = `http://localhost:${port}`;
 const prodUrl = "";
 
+console.log("dev url: ", devUrl);
 const API_URL = process.env.NODE_ENV === "production" ? prodUrl : devUrl;
 
 const cfg = {
